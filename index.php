61d527098554ed89fe15701e5095aa37494f0da7
Wynter Woods <zerotri@gmail.com>
2011-05-25 19:32:22 -0700

    ignoring config file

--- index.php
+++ index.php
@@ -1,72 +1,8 @@
 <?php
 include("windowpane.php");
-include("library/conjugation.php");
-function debug_func($object, $function, $vars = null)
-{
-	global $windowpane;
-	if($windowpane->Config['Environment']['Debug'] == false)
-		return;
-	echo "<pre>";
-	echo get_class($object)."::$function()";
-	if($vars != null)
-	Print_r($vars);
-	echo "</pre>";
-}
-function __autoload($className) {
-	$filePaths = array(
-		0=> Windowpane::getRootDirectory() . "/library/" . $className . ".class.php",
-		1=> Windowpane::getRootDirectory() . "/library/" . $className . ".inc.php",
-		2=> Windowpane::getRootDirectory() . "/app/".$className."/" . $className . ".php");
-	foreach($filePaths as $path)
-	{
-		if(file_exists($path))
-		{
-			include($path);
-			return;
-		}
-	}
-	die("<br>Could not load class $className.<br>");
-}
-
-function setReporting() {
-	if (DEVELOPMENT_ENVIRONMENT == true) {
-		error_reporting(E_ALL);
-		ini_set('display_errors','On');
-	} else {
-		error_reporting(E_ALL);
-		ini_set('display_errors','Off');
-		ini_set('log_errors', 'On');
-		ini_set('error_log', ROOT.'/tmp/logs/error.log');
-	}
-}
-
-function catchExceptions($exception)
-{
-	$error = Array();
-	$error['title'] = "A PHP Exception Occurred!";
-	$error['string'] = $exception->getMessage();
-	$error['file'] = $exception->getFile();
-	$error['code'] = Print_r($exception->getCode(),true);
-	$error['backtrace'] = $exception->getTraceAsString();
-	ob_start();
-    include(dirname(__FILE__)."/public/error.html");
-    echo ob_get_clean();
-}
-set_exception_handler('catchExceptions');
 global $windowpane;
 $windowpane = new Windowpane();
 $url = "posts/view/1";
 
 $windowpane->direct($url);
-
-//
-/*$postView->setFile(Windowpane::getRootDirectory()."/app/v/posts.tpl");
-$postData = $postModel->loadData();
-$postView->posts = $postData;
-$renderedPosts = $postView->render();
-
-//Set view variables
-$pageView->posts = $renderedPosts;
-$pageView->title = "Template Test";*/
-
 ?>
--- windowpane.php
+++ windowpane.php
@@ -1,4 +1,56 @@
 <?php
+include("library/Windowpane.php");
+function debug_func($object, $function, $vars = null)
+{
+	global $windowpane;
+	if($windowpane->Config['Environment']['Debug'] == false)
+		return;
+	echo "<pre>";
+	echo get_class($object)."::$function()";
+	if($vars != null)
+	Print_r($vars);
+	echo "</pre>";
+}
+function __autoload($className) {
+	$filePaths = array(
+		0=> Windowpane::getRootDirectory() . "/library/" . $className . ".class.php",
+		1=> Windowpane::getRootDirectory() . "/library/" . $className . ".inc.php",
+		2=> Windowpane::getRootDirectory() . "/app/".$className."/" . $className . ".php");
+	foreach($filePaths as $path)
+	{
+		if(file_exists($path))
+		{
+			include($path);
+			return;
+		}
+	}
+	die("<br>Could not load class $className.<br>");
+}
+
+function setReporting() {
+	if (DEVELOPMENT_ENVIRONMENT == true) {
+		error_reporting(E_ALL);
+		ini_set('display_errors','On');
+	} else {
+		error_reporting(E_ALL);
+		ini_set('display_errors','Off');
+		ini_set('log_errors', 'On');
+		ini_set('error_log', ROOT.'/tmp/logs/error.log');
+	}
+}
+
+function catchExceptions($exception)
+{
+	$error = Array();
+	$error['title'] = "A PHP Exception Occurred!";
+	$error['string'] = $exception->getMessage();
+	$error['file'] = $exception->getFile();
+	$error['code'] = Print_r($exception->getCode(),true);
+	$error['backtrace'] = $exception->getTraceAsString();
+	ob_start();
+    include(dirname(__FILE__)."../public/error.html");
+    echo ob_get_clean();
+}
 function errorHandler($error_level, $error_message, $error_file, $error_line, $error_context)
 {
 	$type = "";
@@ -13,12 +65,6 @@ function errorHandler($error_level, $error_message, $error_file, $error_line, $e
 		case E_ALL: $type="All"; break;
 		default: $type="Unknown Error"; break;
 	}
-	/*$error  = '</span>';
-	$error .= "<strong><b>$type:</b></strong> $error_message on Line</b> $error_line <b>" .
-		"in file [ </b> $error_file <b>]</b>";
-	$error .= "<br>Backtrace:<pre>";
-	$error .= Print_r(debug_backtrace(),true);
-	$error .= "</pre>";*/
 	$error = Array();
 	$error['title'] = "A PHP Error Occurred!";
 	$error['string'] = $error_message;
@@ -26,229 +72,11 @@ function errorHandler($error_level, $error_message, $error_file, $error_line, $e
 	$error['code'] = $error_context;
 	$error['backtrace'] = Print_r(debug_backtrace(),true);
     ob_start();
-    include(dirname(__FILE__)."/public/error.html");
+    include(dirname(__FILE__)."../public/error.html");
     echo ob_get_clean();
 	die();
 }
 set_error_handler("errorHandler");
+set_exception_handler('catchExceptions');
 
-include('library/WHook.class.php');
-include('library/WObject.class.php');
-include('library/datastore/WDatastore.class.php');
-include('library/datastore/WMySQLDatabase.class.php');
-include('library/WUtil.class.php');
-include('library/WModel.class.php');
-include('library/WView.class.php');
-//include("config/routing.php");
-include("config/inflection.php");
-
-class Windowpane
-{
-	public $Config;
-	public $db;
-	public $Platform;
-	static public function GetUserPlatform()
-	{
-		$oses = array(
-			'/macintosh|mac os x/i' => 'Mac',
-			'/linux/i' => 'Linux',
-			'/windows|win32/i' => 'Windows',
-			'/android/i' => 'Android',
-			'/iOS/i' => 'iOS'
-			);
-		$engines = array(
-			'/webkit/i' => 'WebKit',
-			'/gecko/i' => 'Gecko',
-			'/trident/i' => 'Trident'
-			);
-		$browserStrings = array(
-			'/Chrome/i'		=>	"Chrome",
-			'/Safari/i'		=>	"Safari",
-			'/Firefox/i'	=>	"Firefox",
-			'/Opera/i'		=>	"Opera",
-			'/MSIE/i'		=>	"InternetExplorer",
-			'/Netscape/i'	=>	"Netscape"
-			);
-		$userAgent = $_SERVER['HTTP_USER_AGENT']; 
-		$browser = "Unknown";
-		$os = "Unknown";
-		$platform = "Desktop";
-		$engine = "Unknown";
-
-		//Match browsers
-		foreach ( $browserStrings as $pattern => $result )
-		{
-			if ( preg_match( $pattern, $userAgent ) )
-			{
-				$browser = $result;
-				break;
-			}
-		}
-
-		//Match oses
-		foreach ( $oses as $pattern => $result )
-		{
-			if ( preg_match( $pattern, $userAgent ) )
-			{
-				$os = $result;
-				break;
-			}
-		}
-		//determine platform based on os
-		if($os == 'Android' || $os == 'iOS')
-		{
-			$platform = 'Mobile';
-		}
-		
-		//Match engines
-		foreach ( $engines as $pattern => $result )
-		{
-			if ( preg_match( $pattern, $userAgent ) )
-			{
-				$engine = $result;
-				break;
-			}
-		}
-		//determine engine based on browser, in the event that $engines is wrong
-		if($browser == 'Chrome' || $browser == 'Safari')
-		{
-			$engine = 'WebKit';
-		}
-		
-		return array(
-			'userAgent'	=>	$userAgent,
-			'browser'	=>	$browser,
-			'platform'	=>	$platform,
-			'os'		=>	$os,
-			'engine'	=>	$engine
-			);
-	}
-	static public function MakePlatformSpecificFilenames($filePath)
-	{
-		//input 'public/theme/expression/images/style.less'
-		//and it will return:
-		//array(
-		//		0 => 'public/theme/expression/images/style-mobile-chrome.less',
-		//		1 => 'public/theme/expression/images/style-mobile.less',
-		//		2 => 'public/theme/expression/images/style-chrome.less',
-		//		2 => 'public/theme/expression/images/style.less'
-		//		);
-	}
-	static public function GetRootDirectory()
-	{
-		return dirname(__FILE__);
-	}
-	public function __construct()
-	{
-		include(Windowpane::getRootDirectory().'/config/config-defaults.php');
-		$this->Config = $Config;
-		$this->db = new WMySQLDatabase();
-		$currentPhase = $this->Config['Environment']['Phase'];
-		$this->db->connect(
-			$this->Config['Database'][$currentPhase]['Server'],
-			$this->Config['Database'][$currentPhase]['Username'],
-			$this->Config['Database'][$currentPhase]['Password'],
-			$this->Config['Database'][$currentPhase]['Database']
-			);
-		$this->Platform = Windowpane::GetUserPlatform();
-	}
-	public function __destruct()
-	{
-		$this->db->disconnect();
-	}
-	public function direct($url)
-	{
-		$urlArray = array();
-		$urlArray = explode("/",$url);
-		$controller = $urlArray[0];
-		$action = $urlArray[1];
-		$queryString = $urlArray[2];
-		$controllerName = $controller;
-		$controller = ucwords($controller);
-		$model = rtrim($controller, 's');
-		//$controller .= "Controller";
-		//$controller = $model;
-
-		//Generate view
-		$pageView = $this->generate("View");
-
-		//Set view paths
-		$pageView->setFile("/app/index.tpl");
-		
-		$dispatch = new $controller($model,$controllerName,$action);
-
-		$pageView->content = $this->dispatch_with_fallback($dispatch, $action.$this->Platform['browser'], $action, $queryString);
-		$pageView->title = $controller;
-		
-		//render view
-		echo $pageView->render();
-	}
-	public function dispatch($object, $function, $data)
-	{
-		if ((int)method_exists(get_class($object), $function))
-		{
-			return call_user_func(array($object,$function),$data);
-		}
-		else
-		{
-			/* Error Generation Code Here */
-		}
-	}
-	public function dispatch_with_fallback($object, $function, $fallback, $data)
-	{
-		if ((int)method_exists(get_class($object), $function))
-		{
-			return call_user_func(array($object,$function),$data);
-		}
-		else
-		{
-			if ((int)method_exists(get_class($object), $fallback))
-			{
-				return call_user_func(array($object,$fallback),$data);
-			}
-			else
-			{
-				/* Error Generation Code Here */
-			}
-		}
-	}
-	public function generate($type)
-	{
-		//if($name != null)
-		{
-			switch($type)
-			{
-				case "View":
-					return new WView();
-					break;
-				case "Model":
-					return new WModel();
-					break;
-				case "SQL":
-					return new WMySQLDatabase();
-					break;
-			}
-		}
-		/*else
-		{
-			switch($type)
-			{
-				case "Controller":
-					$controllerFile = $name . ".controller.php";
-					if(file_exists(ROOT."/app/c/".$controllerFile))
-					{
-						require_once(ROOT."/app/c/".$controllerFile);
-					}
-					break;
-				case "View":
-					break;
-				case "Model":
-					break;
-				case "SQL":
-					return new WMySQL();
-					break;
-			}
-		}*/
-	}
-}
 ?>
